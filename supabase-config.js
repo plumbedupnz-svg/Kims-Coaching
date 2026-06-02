d4d00981c1647e8340d5be67b07b5b490d964d8c
plumbedup-wq <plumbed.up@vodafone.co.nz>
2026-06-02 12:21:45 +1200

    Fix Kims Supabase project URL

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.KIMS_SUPABASE = window.KIMS_SUPABASE || {
-  url: "https://rmojzwlmefoomvdpxkls.supabase.co",
+  url: "https://gvcirppexadpgobwmkcf.supabase.co",
   anonKey: "sb_publishable_XPi428GP9d8ZaSrbWdXFTw_KtPB6_KF"
 };
